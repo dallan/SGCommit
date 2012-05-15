1583ec625c50148265fb3fed5f41c2dd70c6334f
Dan Allan <usallans@netscape.net>
2012-05-14 22:26:47 -0600

    Initial SGCommit commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SGCommit::Application.config.secret_token = '0aa99a14385bb712be909c968d3f176628ea72fb45033bb298111b7b3c95dd669d1e94d8a8ce8eb6b1c51cf13d22a9839bd9c09a996b18fea4cbafdda101f761'
